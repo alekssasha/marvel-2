6710bc10253de7b2c536ec22eb53adc5c7ddf1d2
Aleks <example@email.com>
2021-12-14 20:53:27 +0400

    before transform finished

---
+++ src/components/randomChar/RandomChar.js
@@ -0,0 +1,77 @@
+import { Component } from 'react/cjs/react.production.min';
+import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png';
+import MarvelService from '../../services/MarvelService';
+
+class RandomChar extends Component {
+	constructor(props) {
+		super(props);
+		this.updateChair();
+	}
+	state = {
+		name: null,
+		description: null,
+		thumbnail: null,
+		pagehome: null,
+		wiki: null
+	}
+
+	marvelService = new MarvelService;
+
+	updateChair = () => {
+		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+		this.marvelService
+			.getCharachter(id)
+			.then(res => {
+				this.setState({
+					name: res.data.results[0].name,
+					description: res.data.results[0].description,
+					thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+					pagehome: res.data.results[0].urls[0].url,
+					wiki: res.data.results[0].urls[1].url
+				})
+			})
+	}
+
+   render() {
+
+	const {name, description, thumbnail, pagehome, wiki} = this.state;
+
+
+    return (
+        <div className="randomchar">
+            <div className="randomchar__block">
+                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                <div className="randomchar__info">
+                    <p className="randomchar__name">{name}</p>
+                    <p className="randomchar__descr">{description}
+                    </p>
+                    <div className="randomchar__btns">
+                        <a href={pagehome} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="randomchar__static">
+                <p className="randomchar__title">
+                    Random character for today!<br/>
+                    Do you want to get to know him better?
+                </p>
+                <p className="randomchar__title">
+                    Or choose another one
+                </p>
+                <button className="button button__main">
+                    <div className="inner">try it</div>
+                </button>
+                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+            </div>
+        </div>
+    )
+   }
+}
+
+export default RandomChar;
